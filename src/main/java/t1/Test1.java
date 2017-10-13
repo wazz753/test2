851b36379a3368e0e10e3fd918bd6644e7f23aed
wazz753 <wazz753@126.com>
2017-10-13 10:51:37 +0800

    提交6

--- src/main/java/t1/Test1.java
+++ src/main/java/t1/Test1.java
@@ -3,16 +3,6 @@ package t1;
 public class Test1 {
 
 	public static void main(String[] args) {
-
 		System.out.println("cccc");
-		System.out.println("ddd");
-		int cc=3;
-		int cc2=3;
-		int cc4=51;
-		
-		int a=1;
-		System.out.println("bbb");
-		int c=2;
-
 	}
 }
