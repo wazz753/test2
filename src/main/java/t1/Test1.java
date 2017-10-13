9bc83cf2bc8f66bf5ed846016acf6f8e003704a4
wazz753 <wazz753@126.com>
2017-10-13 10:45:16 +0800

    555555

--- src/main/java/t1/Test1.java
+++ src/main/java/t1/Test1.java
@@ -3,18 +3,16 @@ package t1;
 public class Test1 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("cccc");
 		System.out.println("ddd");
 		int cc=3;
 		int cc2=3;
 		int cc4=51;
-=======
-
+		
 		int a=1;
 		System.out.println("bbb");
 		int c=2;
 
->>>>>>> branch 'master' of https://github.com/wazz753/test2.git
 	}
 }
