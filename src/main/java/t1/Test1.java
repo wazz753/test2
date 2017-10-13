445be36803c2188669d8e9d05b9da9cd552cdf32
wazz753 <wazz753@126.com>
2017-10-13 10:49:38 +0800

    桌面修改3

--- src/main/java/t1/Test1.java
+++ src/main/java/t1/Test1.java
@@ -4,15 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("cccc");
-		System.out.println("ddd");
-		int cc=3;
-		int cc2=3;
-		int cc4=51;
+		int j=0;
 		
-		int a=1;
-		System.out.println("bbb");
-		int c=2;
+		
 
 	}
 }
